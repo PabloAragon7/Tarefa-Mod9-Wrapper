45dbd0d34e23257e1e6476f66d0801fb8cf66f9e
Pablo Câmara Aragon <pabloaragon7@gmail.com>
2022-07-07 15:02:54 -0300

    Valornumérico.java

---
+++ Valornumérico.java
@@ -0,0 +1,17 @@
+/**
+ * @author pablo
+ */
+
+public class Valornumérico {
+
+    public static void main(String args[]){
+        System.out.println("** Conversao int - Integer **");
+        int valorNum1 = 7;
+        System.out.println(valorNum1);
+        Integer valorNum2=valorNum1;
+        System.out.println(valorNum2);
+
+
+    }
+
+}
